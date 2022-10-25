079e16dbf84bfd8d2ff0291571a5041807e51d09
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2022-10-25 09:30:34 +0200

    fix: version word spelling

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Vrsion {packageJson.version}</h3>
+        <h3>Version {packageJson.version}</h3>
       </header>
     </div>
   );
